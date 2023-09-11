76e2ea6d8938f6c6896e7a78d6ceb0179a41fbfc
dabruge <davidbrucegeddes@gmail.com>
2023-09-11 14:58:10 +0100

    create_recipe: Add functionality for multiple ingredients

--- src/recipes.py
+++ src/recipes.py
@@ -1,3 +1,4 @@
+import os
 import time
 from ingredients import Ingredient, create_ingredient, list_ingredients, edit_unit
 
@@ -14,24 +15,33 @@ class Recipe:
 
 
 def create_recipe(recipe_list, ingredient_list):
+    # might need to refactor so same ingredient cant appear in receipe twice
+    add_more_ingr = True
     recipe_name = input("Enter recipe name:\n").lower()
-    if [rec for rec in recipe_list if rec.name == recipe_name]:
-        print(f"\nRecipe '{recipe_name}' already exists!")
-        time.sleep(1.5)
-    else:
-        new_recipe = Recipe(recipe_name)
-        ingr_name = input("\nEnter ingredient:\n")
-        existing_ingr = [ingr for ingr in ingredient_list if ingr.name == ingr_name]
-        if existing_ingr:
-            ingr_qty = input(f"\nEnter quantity for {ingr_name} (in {existing_ingr[0].unit}):\n")
-            new_recipe.add_ingredient(existing_ingr[0], ingr_qty)
+    while add_more_ingr:
+        choice = ''
+        if [rec for rec in recipe_list if rec.name == recipe_name]:
+            print(f"\nRecipe '{recipe_name}' already exists! Use the Edit recipe option")
+            time.sleep(1.5)
         else:
-            # ingredient does not exist
-            print(f"\nIngredient '{ingr_name}' does not exist! Create ingredient now:\n")
-            new_ingredient = create_ingredient(ingredient_list)
-            ingr_qty = input(f"\nEnter quantity for {ingr_name} (in {new_ingredient.unit}):\n")
-            new_recipe.add_ingredient(new_ingredient, ingr_qty)
+            new_recipe = Recipe(recipe_name)
+            ingr_name = input("\nEnter ingredient:\n")
+            existing_ingr = [ingr for ingr in ingredient_list if ingr.name == ingr_name]
+            if existing_ingr:
+                ingr_qty = input(f"\nEnter quantity for {ingr_name} (in {existing_ingr[0].unit}):\n")
+                new_recipe.add_ingredient(existing_ingr[0], ingr_qty)
+            else:
+                # ingredient does not exist
+                print(f"\nIngredient '{ingr_name}' does not exist! Create ingredient now:\n")
+                new_ingredient = create_ingredient(ingredient_list)
+                ingr_qty = input(f"\nEnter quantity for {ingr_name} (in {new_ingredient.unit}):\n")
+                new_recipe.add_ingredient(new_ingredient, ingr_qty)
 
-
-        new_recipe.add_to_list(recipe_list)
+            while choice not in ['y', 'n']:
+                os.system('clear')
+                print(f"{new_ingredient.name} added for {new_recipe.name}\n")
+                choice = input("Add another ingredient? [y/n]\n").lower()
+                if choice == 'n':
+                    add_more_ingr = False
+    new_recipe.add_to_list(recipe_list)
 
